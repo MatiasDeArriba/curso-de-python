9aa63852e9601e552520477c02a8603d4acf7dfd
matiasdearriba <matiasdearriba10@gmail.com>
2026-06-03 11:20:38 -0300

    actividades

---
+++ operaciones_numericas.py
@@ -0,0 +1,8 @@
+# Aritméticos básicos
+10 + 3    # 13
+10 - 3    # 7
+10 * 3    # 30
+10 / 3    # 3.333...  → siempre devuelve float
+10 // 3   # 3         → división entera
+10 % 3    # 1         → resto (módulo)
+10 ** 3   # 1000      → potencia
