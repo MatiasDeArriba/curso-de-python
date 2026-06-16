80ed348c1f09cb1f505528cf956c3bcc26d14df4
matiasdearriba <matiasdearriba10@gmail.com>
2026-06-16 13:45:39 -0300

    actualizo curso

--- primeras_funciones.py
+++ primeras_funciones.py
@@ -3,3 +3,11 @@ def mensaje(): # Definición de la función llamada "mensaje"
     print("¡Bienvenido a la programación en Python!") # CUERPO DE LA FUNCION: Código que se ejecutará cuando se llame a la función
     print("¡Diviértete aprendiendo a programar!") # CUERPO DE LA FUNCION: Código que se ejecutará cuando se llame a la función
 mensaje() # Llamada a la función para ejecutar el código dentro de ella
+
+
+def suma():
+    num1 = 5 # Variable local dentro de la función
+    num2 = 7 # Variable local dentro de la función
+    print(num1 + num2) # Imprime el resultado de la suma
+
+suma() # Llamada a la función para ejecutar el código dentro de ella
