73770d41261c39de4df08fc4ef5760fab87ed90b
matiasdearriba <matiasdearriba10@gmail.com>
2026-06-16 13:37:58 -0300

    ACTUALIZO

---
+++ primeras_funciones.py
@@ -0,0 +1,5 @@
+def mensaje(): # Definición de la función llamada "mensaje"
+    print("¡Hola, mundo!") # CUERPO DE LA FUNCION: Código que se ejecutará cuando se llame a la función
+    print("¡Bienvenido a la programación en Python!") # CUERPO DE LA FUNCION: Código que se ejecutará cuando se llame a la función
+    print("¡Diviértete aprendiendo a programar!") # CUERPO DE LA FUNCION: Código que se ejecutará cuando se llame a la función
+mensaje() # Llamada a la función para ejecutar el código dentro de ella
