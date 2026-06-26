f95de50bfbe1fa88c29de091b0b6ef54397cc7ea
matiasdearriba <matiasdearriba10@gmail.com>
2026-06-26 10:50:26 -0300

    update

---
+++ ejercicios_clases/retos_actividades1.py
@@ -0,0 +1,24 @@
+#quiz
+
+signal = "green" #variable que almacena el color de la señal de tráfico
+if signal == "red": #si la señal es roja
+    print("Stop") #imprime "Stop"
+elif signal == "yellow": #si la señal es amarilla
+    print("Wait") #imprime "Wait" que significa "espera" en inglés
+else:
+    print("Go")
+
+#imprime GO
+
+signal = "Green" #variable que almacena el color de la señal de tráfico
+
+if signal == "Red": #Si la señal es roja
+    print("Stop") #Imprime "Stop"
+elif signal == "Yellow": #si la señal es amarilla
+    print("Wait") #imprime "Wait" que significa "espera" en inglés
+elif signal == "Green": #si la señal es verde
+    print("Go") #imprime "Go" que significa "adelante" en inglés
+else: # si la señal no es ninguna de las anteriores
+    print("Señal inválida") #imprime "Señal inválida" que significa "invalid signal" en inglés
+    
+ #imprime GO
